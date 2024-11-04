8983f96572a78dd285f92dded2be2e93ba9f7606
juan cuadra <jfcocuadra@gmail.com>
2024-11-04 20:47:07 -0300

    inicializando parcial front3

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div className="App">
+        <h1>Carga de estudiantes </h1>
+        <form> </form>
+      <Card/>
+      </div>
+    
+  )
+}
+
+export default App
